f4029bd0994f21b283b27cd1adf587196d24ee30
Kevin Conklin <kevin_william_conklin@yahoo.com>
2018-10-15 15:01:17 -0700

    Update Assets/Scripts/Classes/LevelConfig.cs

--- Assets/Scripts/Classes/LevelConfig.cs
+++ Assets/Scripts/Classes/LevelConfig.cs
@@ -30,6 +30,7 @@ namespace Game.Interface
     /// <summary>
     /// Config for each level setup
     /// </summary>
+    [System.Serializable]
     public struct LevelConfig
     {
         /// <summary>
